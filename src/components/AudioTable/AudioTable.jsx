1712f2d8be33a18f8df34cce0ee837212bf082af
PommesPeter <me@pommespeter.space>
2024-05-09 19:56:54 +0800

    added audio demos

--- src/components/AudioTable/AudioTable.jsx
+++ src/components/AudioTable/AudioTable.jsx
@@ -1,47 +1,45 @@
-import clsx from 'clsx';
 import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
 
 import styles from './AudioTable.module.css';
 
 const AudioList = [
     {
-        prompt: "Semiautomatic gunfire occurs with slight echo",
+        prompt: "Semiautomatic gunfire occurs with slight echo.",
         auUrl: "/audios/semiautomatic_gunfire_occurs_with_slight_echo.wav",
         augtUrl: "/audios/semiautomatic_gunfire_occurs_with_slight_echo_gt.wav"
     },
     {
-        prompt: "A telephone bell rings",
+        prompt: "A telephone bell rings.",
         auUrl: "/audios/a_telephone_bell_rings.wav",
         augtUrl: "/audios/a_telephone_bell_rings_gt.wav"
     },
     {
-        prompt: "An engine running followed by",
+        prompt: "An engine running followed by the engine revving and tires screeching.",
         auUrl: "/audios/an_engine_running_followed_by_the_engine_revving_and_tires_screeching.wav",
         augtUrl: "/audios/an_engine_running_followed_by_the_engine_revving_and_tires_screeching_gt.wav"
     },
     {
-        prompt: "Birds chirping with insects b",
+        prompt: "Birds chirping with insects buzzing and outdoor ambiance.",
         auUrl: "/audios/birds_chirping_with_insects_buzzing_and_outdoor_ambiance.wav",
         augtUrl: "/audios/birds_chirping_with_insects_buzzing_and_outdoor_ambiance_gt.wav"
     },
     {
-        prompt: "Water trickling rapidly and d",
+        prompt: "Water trickling rapidly and draining.",
         auUrl: "/audios/water_trickling_rapidly_and_draining.wav",
         augtUrl: "/audios/water_trickling_rapidly_and_draining_gt.wav"
     },
     {
-        prompt: "Thunderstorm sounds while rai",
+        prompt: "Thunderstorm sounds while raining.",
         auUrl: "/audios/thunderstorm_sounds_while_raining.wav",
         augtUrl: "/audios/thunderstorm_sounds_while_raining_gt.wav"
     },
     {
-        prompt: "Birds chirping repeatedly",
+        prompt: "Birds chirping repeatedly.",
         auUrl: "/audios/birds_chirping_repeatedly.wav",
         augtUrl: "/audios/birds_chirping_repeatedly_gt.wav"
     },
     {
-        prompt: "Several large bells ring",
+        prompt: "Several large bells ring.",
         auUrl: "/audios/Several_large_bells_ring.wav",
         augtUrl: "/audios/Several_large_bells_ring_gt.wav"
     },
